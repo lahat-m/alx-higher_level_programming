80f08c725ed3cb1d772b7367083852c688463ea1
lahat-m <lahat-m>
2022-08-23 19:42:42 +0000

    Print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -7,13 +7,4 @@ def print_last_digit(number):
         l_digit *= -1
 
     print("{:d}".format(l_digit), end='')
-    return (l_digit)#!/usr/bin/python3
-def print_last_digit(number):
-    if number >= 0:
-        l_digit = number % 10
-    else:
-        l_digit = number % -10
-        l_digit *= -1
-
-    print("{:d}".format(l_digit), end='')
     return (l_digit)
