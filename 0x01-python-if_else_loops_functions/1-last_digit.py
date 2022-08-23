93138e37969ca84b6c8c6c0eea8dadeb77fd0fb1
lahat-m <lahat-m>
2022-08-23 19:24:55 +0000

    Print last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,4 @@
-mport random
+import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10 if number >= 0 else ((-number % 10) * -1)
 message = f"Last digit of {number} is {last_digit}"
